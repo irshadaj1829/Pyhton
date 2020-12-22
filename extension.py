d4d157c5ef54e88a86e1a2b7abb120acb5af2e4c
MUHAMMED IRSHAD A J <55504536+irshadaj1829@users.noreply.github.com>
2020-12-22 12:38:18 +0530

    accept file name and print extension

---
+++ extension.py
@@ -0,0 +1,8 @@
+filename = input("Input the Filename: ")
+f_extns = filename.split(".")
+print ("The extension of the file is : " + repr(f_extns[-1]))
+
+
+""" output
+Input the Filename: abc.py
+The extension of the file is : 'py' """
