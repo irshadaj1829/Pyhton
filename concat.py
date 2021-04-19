89133828a41f857e5c09d3e702f4c9d2f4c42efb
MUHAMMED IRSHAD A J <55504536+irshadaj1829@users.noreply.github.com>
2021-04-20 00:10:24 +0530

    String concatination

---
+++ concat.py
@@ -0,0 +1,12 @@
+#string concatination
+
+adjective = "beatuiful"
+
+# 3 methods for string concatination 
+# date 19/04/2021
+
+print("you are "+ adjective)
+
+print("you are {} ".format(adjective))
+
+print(f"you are so {adjective}")
